7d69e59bff2c025795527de8f45ee7d9380bf8f4
rog14 <jacob@rog.14>
2025-02-19 19:56:21 -0800

    integrate nvidia specialisation into default config

--- nix/rog14/rog14.nix
+++ nix/rog14/rog14.nix
@@ -12,26 +12,19 @@
   #gpu
   #disable discrete gpu by default
   hardware.graphics.enable = true;
-  hardware.nvidiaOptimus.disable = true;
-
-  #specialisation that enables dgpu
-  specialisation = {
-    nvidia.configuration = {
-      hardware.nvidiaOptimus.disable = lib.mkForce false;
-      services.xserver.videoDrivers = ["amdgpu" "nvidia"];
-      hardware.nvidia = {
-        modesetting.enable = true;
-        dynamicBoost.enable = true;
-        powerManagement.enable = true;
-        powerManagement.finegrained = true;
-        open = false;
-        prime = {
-          offload.enable = true;
-          offload.enableOffloadCmd = true;
-          nvidiaBusId = "PCI:1:0:0";
-          amdgpuBusId = "PCI:4:0:0";
-        };
-      };
+  hardware.nvidiaOptimus.disable = lib.mkForce false;
+  services.xserver.videoDrivers = ["amdgpu" "nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    dynamicBoost.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+    open = false;
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:4:0:0";
     };
   };
 
@@ -60,7 +53,6 @@
   };
   powerManagement.powertop.enable = true;
   networking.networkmanager.wifi.powersave = true;
-  networking.networkmanager.wifi.backend = "iwd";
   #steam
   programs.steam.enable = true;
   programs.steam.extraCompatPackages = [pkgs.proton-ge-bin];
