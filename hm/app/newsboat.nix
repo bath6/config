45bfd952bf0c043b7ab07465f8b0096e820b2586
rog14 <jacob@rog.14>
2025-02-21 10:43:30 -0800

    newsboat new urls

--- hm/app/newsboat.nix
+++ hm/app/newsboat.nix
@@ -6,8 +6,8 @@
 
     extraConfig = ''
       color info black cyan
-      color listfocus green black
-      color listfocus_unread green black
+      color listfocus black green
+      color listfocus_unread black green
     '';
 
     urls = [
@@ -18,6 +18,18 @@
       {
         tags = ["podcast"];
         url = "https://www.omnycontent.com/d/playlist/aaea4e69-af51-495e-afc9-a9760146922b/4a3ca742-9a68-4850-a727-ab790176c0e9/d04153b0-fc4c-4191-ab18-ab790179c563/podcast.rss";
+      }
+      {
+        title = "---Youtube---";
+        url = "";
+      }
+      {
+        tags = ["youtube"];
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UChbVFLcb9f5L6oZaGtztbCg";
+      }
+      {
+        tags = ["youtube"];
+        url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC_O58Rr2DOskJvs9bArpLkQ";
       }
     ];
   };
